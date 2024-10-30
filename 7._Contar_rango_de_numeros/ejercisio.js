39a575f99b0245d0d78ca8422809bc645e2bedf4
moisespenafiel10 <penafieltellomoi@gmail.com>
2024-10-30 12:56:04 -0500

    feat:solucion del ejercicio 7

---
+++ 7._Contar_rango_de_numeros/ejercisio.js
@@ -0,0 +1,17 @@
+// escribe tu respuesta acá
+
+function contarRango(inferior, superior) {
+    let contador = 0; 
+
+    
+    for (let i = inferior + 1; i < superior; i++) {
+        contador++; 
+    }
+
+    return contador; 
+}
+
+// código de prueba
+console.log(contarRango(1, 9)) // 7
+console.log(contarRango(1332, 8743)) // 7410
+console.log(contarRango(5, 6)) // 0
